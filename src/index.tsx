f06966408180b5040ed69720476dbe5f89a60a3a
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-09-02 17:38:58 +0300

    change sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { myPersistStore, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import './components/index.scss'
+import './sass/index.scss'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
