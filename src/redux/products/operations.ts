e12d701a4e21ca535a96b5853f8f0057078472ce
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-09-06 22:42:42 +0300

    add

--- src/redux/products/operations.ts
+++ src/redux/products/operations.ts
@@ -1,17 +1,22 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.escuelajs.co/api/v1';
+axios.defaults.baseURL = 'https://api.escuelajs.co/api/v1/';
 
 interface IProductData {
   data: object[];
+}
+
+interface Price {
+  min: number;
+  max: number;
 }
 
 export const categories = createAsyncThunk<IProductData>(
   'products',
   async (_, { rejectWithValue }) => {
     try {
-      const response = await axios.get<IProductData>('/categories');
+      const response = await axios.get<IProductData>('categories');
       
       return response.data;
     } catch (error: any) {
@@ -24,7 +29,7 @@ export const productsByName = createAsyncThunk<IProductData, string>(
   'productsByName',
   async (name, { rejectWithValue }) => {
     try {
-      const response = await axios.get<IProductData>(`/products/?title=${name}`);
+      const response = await axios.get<IProductData>(`products/?title=${name}`);
       console.log(response.data);
       
       return response.data;
@@ -33,3 +38,32 @@ export const productsByName = createAsyncThunk<IProductData, string>(
     }
   }
 );
+
+export const productsByPrice = createAsyncThunk<IProductData, Price>(
+  'productsByPrice',
+  async (price, { rejectWithValue }) => {
+    try {
+      const response = await axios.get<IProductData>(`products/?price_min=${price.min}&price_max=${price.max}`);
+      console.log(response.data);
+      
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const productsByCategory = createAsyncThunk<IProductData, number>(
+  'productsByCategory',
+  async (Id, { rejectWithValue }) => {
+    try {
+      const response = await axios.get<IProductData>(`products/?categoryId=${Id}`);
+      console.log(response.data);
+      
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
